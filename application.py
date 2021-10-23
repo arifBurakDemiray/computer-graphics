083587f923fe730ca5c5b9f69fd156f25125cfab
arifBurakDemiray <burakdemiray09@hotmail.com>
2021-10-23 16:05:42 +0300

    feat: mat3d class

--- application.py
+++ application.py
@@ -1,2 +1,7 @@
+# CENG 487 Assignment1 by
+# Arif Burak Demiray
+# StudentId: 250201022
+# October 2021
+
 from vec3d import vec3d
 x = vec3d(1,5,8,1)
---
+++ mat3d.py
@@ -0,0 +1,10 @@
+# CENG 487 Assignment1 by
+# Arif Burak Demiray
+# StudentId: 250201022
+# October 2021
+
+class mat3d:
+    content = []
+
+    def __init__(self,content: list[int]) -> 'mat3d':
+        self.content = content
