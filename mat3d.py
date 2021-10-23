ce4c4c49572f810b6e54c0263c9251eeb2ee4162
arifBurakDemiray <burakdemiray09@hotmail.com>
2021-10-23 17:20:07 +0300

    feat: transpose function for the matrix

--- mat3d.py
+++ mat3d.py
@@ -4,7 +4,26 @@
 # October 2021
 
 class mat3d:
+    """
+    This class holds a 4x4 matrix
+
+    Please give matrix as an array because of memory access
+
+    [4][1] in matrix
+    width(row)*4 + 1 in array implementation
+
+    so in the matrix [x,y] is equal to width(row)*y + x in the array
+    """
     content = []
 
     def __init__(self,content: list[int]) -> 'mat3d':
         self.content = content
+
+    def calc_transpose(self) -> 'mat3d':
+        value = []
+
+        for i in range(4):
+            for y in range(4):
+                value.append(self.content[y*4 + i])
+
+        return mat3d(value)
