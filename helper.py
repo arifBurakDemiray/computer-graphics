6d701e1a92ace217c82102c52f26612ff8228307
arifBurakDemiray <burakdemiray09@hotmail.com>
2021-10-26 21:58:02 +0300

    chore: comment to helper class

--- helper.py
+++ helper.py
@@ -6,9 +6,31 @@
 from numpy import pi
 
 def degree_to_radian(degree: float) -> float:
+    """
+    This function converts a degree to its radian equivalent
+
+    Parameters:
+
+    Degree
+
+    Returns:
+
+    Radian
+    """
     return degree*pi/180
 
 def radian_to_degree(radian: float) -> float:
+    """
+    This function converts a radian to its degree equivalent
+
+    Parameters:
+
+    Radian
+
+    Returns:
+
+    degree
+    """
     return radian*180/pi
 
 
